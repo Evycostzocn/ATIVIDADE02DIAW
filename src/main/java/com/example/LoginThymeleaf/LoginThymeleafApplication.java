c1f3bf8dd41c4ceda043763211b917e734c722eb
Evelyn Costa <evelyn.costa@quasar.srv.br>
2026-09-12 21:27:09 -0300

    feat: Login e Css

---
+++ src/main/java/com/example/LoginThymeleaf/LoginThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoginThymeleaf/LoginThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
